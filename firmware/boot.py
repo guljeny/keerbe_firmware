a218ce9fb936d7ada63300f03efe9c5a71c48cf6
Yauheni Hul <yauheni.hul@itechart-group.com>
2022-01-09 19:52:07 +0100

    setupe build script first firmware version now PC understand rp2040 as keyboard device fix problem with MacOs wakeup

--- firmware/boot.py
+++ firmware/boot.py
@@ -14,26 +14,18 @@ button = digitalio.DigitalInOut(COLUMN_PINS[0])
 button.direction = digitalio.Direction.INPUT
 button.pull = digitalio.Pull.DOWN
 
-# print(usb_hid.Device.KEYBOARD)
-# usb_hid.enable((usb_hid.Device.CONSUMER_CONTROL), boot_device = 0)
-
 storage.remount("/", readonly = False, disable_concurrent_write_protection = True)
 supervisor.disable_autoreload()
 
 
 if not button.value:
-    print('boot as keyboard')
+    usb_cdc.enable(console=True, data=True)
+    supervisor.set_next_code_file('./flash.py')
+else:
     usb_cdc.enable(console=False, data=False)
     usb_hid.enable((usb_hid.Device.KEYBOARD,usb_hid.Device.CONSUMER_CONTROL), boot_device = 1)
-    # usb_hid.enable((usb_hid.Device.KEYBOARD,), boot_device = 1)
-    # usb_cdc.enable(console=False, data=True)
     storage.disable_usb_drive()
     supervisor.set_next_code_file('./main.py')
-else:
-    print('boot in dev')
-    usb_cdc.enable(console=True, data=True)
-    # usb_hid.enable((usb_hid.Device.KEYBOARD,usb_hid.Device.CONSUMER_CONTROL))
-    supervisor.set_next_code_file('./flash.py')
 
 output.deinit()
 button.deinit()
